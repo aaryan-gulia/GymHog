1ff0252af26117e35175d9950ef8bde3bbd628f7
aaryan_gulia <aaryan.gulia@gmail.com>
2025-08-17 23:39:45 +0100

    surrealDB basic setup and integration

--- src/backend.rs
+++ src/backend.rs
@@ -1,6 +1,38 @@
 #[cfg(feature = "server")]
 use {
-    lazy_static::lazy_static,
     serde::{Deserialize, Serialize},
-    surrealdb::{engine::remote::ws::Ws, opt::auth::Root, opt::Resource, RecordId, Surreal, Value},
+    surrealdb::{
+        engine::remote::ws::{Client, Ws},
+        opt::auth::Root,
+        RecordId, Surreal, Value,
+    },
+    tokio::sync::OnceCell,
 };
+
+#[cfg(feature = "server")]
+static DB: OnceCell<Surreal<Client>> = OnceCell::const_new();
+
+#[cfg(feature = "server")]
+async fn db() -> Surreal<Client> {
+    let db = Surreal::new::<Ws>("127.0.0.1:8000")
+        .await
+        .expect("Can't connect to surrealdb");
+
+    db.signin(Root {
+        username: "root",
+        password: "secret",
+    })
+    .await
+    .expect("Can't signin to surrealdb");
+
+    db.use_ns("test")
+        .use_db("test")
+        .await
+        .expect("Can't use_ns and use_db");
+    db
+}
+
+#[cfg(feature = "server")]
+pub async fn get_db() -> &'static Surreal<Client> {
+    DB.get_or_init(db).await
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 use dioxus::prelude::*;
+use serde::{Deserialize, Serialize};
+use std::dbg;
+
+#[cfg(feature = "server")]
+use surrealdb::RecordId;
 
 pub mod backend;
+
+#[cfg(feature = "server")]
+use backend::get_db;
 
 fn main() {
     dioxus::launch(App);
@@ -26,7 +34,9 @@ fn Programming() -> Element {
         }
         button {
         id : "add_program",
-        onclick : move |_| async move {_ = add_program(program_name.to_string()).await;},
+        onclick : move |_| async move {
+        _ = add_program(ProgramTestModel1 { name: program_name.to_string() }).await;
+        },
         "add program"
         }
     }
@@ -34,7 +44,20 @@ fn Programming() -> Element {
 }
 
 #[server]
-async fn add_program(name: String) -> Result<(), ServerFnError> {
-    println!("server received program : {}", name);
+async fn add_program(program: ProgramTestModel1) -> Result<(), ServerFnError> {
+    let program_added: Option<ProgramTestModel1> = get_db()
+        .await
+        .create("Program")
+        .content(program)
+        .await
+        .expect("Can't add program");
+
+    dbg!(program_added);
+
     Ok(())
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct ProgramTestModel1 {
+    name: String,
 }
